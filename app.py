3d3ae897b980feac3ceebce511959e010f833d7a
unknown <timothycfang55@gmail.com>
2021-11-06 01:09:57 -0500

    table working

--- app.py
+++ app.py
@@ -252,6 +252,7 @@ fig.update_yaxes(showspikes=True)
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 # Embed the style to the dashabord:
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 app.layout = html.Div(
     children=[
