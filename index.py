19f9a6d029900cdff3dd007e1a963066f9404e87
unknown <timothycfang55@gmail.com>
2021-11-17 19:30:01 -0600

    fixed spelling error

--- index.py
+++ index.py
@@ -27,7 +27,7 @@ def display_page(pathname):
     if pathname == '/apps/heroprice':
         return heroprice.layout
     else:
-        return taverdata.layout
+        return taverndata.layout
         #return "404 Page Error! Please choose a link"
 
 if __name__ == '__main__':
